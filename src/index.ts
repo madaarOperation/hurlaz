7d7d715f4e416aadcd3c67ece1ea29cb7d989448
tree-1917 <gamalmo1917@gmail.com>
2026-05-31 11:44:31 +0300

    feat: use core group for orginize output logs

--- src/index.ts
+++ src/index.ts
@@ -20,23 +20,31 @@ class HurlManager {
 
   // INFO: Hurl Package Downloader
   async download(): Promise<void> {
+    core.startGroup("Download Hurl Binary");
     core.info(`Downloading Hurl ${this.version}...`);
+    core.endGroup();
   }
 
   // INFO: Hurl Script Finder
   async find(): Promise<void> {
+    core.startGroup("Find Hurl Scripts");
     core.info(`Finding Hurl files matching "${this.pattern}"...`);
+    core.endGroup();
   }
 
   // INFO: Hurl Script Runner
   async execute(): Promise<void> {
+    core.startGroup("Execute Hurl Scripts");
     core.info("Running Hurl tests...");
     // hurl --variables-file vars.env --test *.hurl -v
+    core.endGroup();
   }
 
   // INFO: Hurl Final Reporter
   async report(): Promise<void> {
+    core.startGroup("Generate Test Report");
     core.info("Generating report...");
+    core.endGroup();
   }
 
   // INFO: Main workflow
