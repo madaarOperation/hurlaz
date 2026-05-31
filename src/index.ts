c433be572ec82766ea9b924ed8cbeb569a2b511d
tree-1917 <gamalmo1917@gmail.com>
2026-05-31 11:15:24 +0300

    update: update index.ts with funcation strcture

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,49 @@
 import * as core from "@actions/core";
 
 // INFO: Hurl Types
-// INFO: Hurl Downloader
-// INFO: Hurl Finder
-// INFO: Hurl Runner
-// INFO: Hurl reporter
+interface HurlManager {
+  version: string;
+  compose: string;
+  pattern: string;
+  threshold: number;
+  myHurlDownloader: (hurlManager: HurlManager) => Promise<void>;
+  myHurlFinder: (HurlManager: HurlManager) => Promise<void>;
+  myHurlRunner: (HurlManager: HurlManager) => Promise<void>;
+  myHurlReporter: (HurlManager: HurlManager) => Promise<void>;
+}
+
+// INFO: Hurl Package Downloader
+async function hurlDownloader(hurlManager: HurlManager): Promise<void> { }
+
+// INFO: Hurl Script Finder
+async function hurlFinder(hurlManager: HurlManager): Promise<void> { }
+
+// INFO: Hurl Script Runner
+async function hurlRunner(hurlManager: HurlManager): Promise<void> { }
+
+// INFO: Hurl Final Reporter
+async function hurlReporter(hurlManager: HurlManager): Promise<void> { }
+
 // INFO: Entrypoint Function
 async function run() {
   try {
-    // 1. Get the input
-    const version = core.getInput("hurl-version");
-    core.info(`Peraring to use Hurl version ${version}`);
-
+    // 1. Create My Hurl Manager
+    const myHurlManger: HurlManager = {
+      version: core.getInput("hurl-version"),
+      compose: core.getInput("compose-path"),
+      pattern: core.getInput("hurl-pattern"),
+      threshold: parseInt(core.getInput("threshold"), 100),
+      myHurlDownloader: hurlDownloader,
+      myHurlFinder: hurlFinder,
+      myHurlRunner: hurlRunner,
+      myHurlReporter: hurlReporter,
+    };
+    // 2. Download Hurl Package
+    // 3. Check Hurl Script Location
+    // 4. Run Hurl Script
+    // 5. Extract Data Build Report
     // 2. Set the output
-    core.setOutput("hurl-version", version);
+    core.setOutput("hurl-version", myHurlManger.version);
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
   }
